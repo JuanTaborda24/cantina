609fb2d87138ddc45515fc3ef2e1604e602455bf
JuanTaborda24 <taborda.juan@escola.pr.gov.br>
2024-11-14 10:55:36 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,29 @@
 
+from flask import Flask, render_template_string, request, redirect, url_for
+import sqlite3 
+
+app = Flask(__name__)
+
+def init_db ():
+    connection = sqlite3.connect ('Cantina_legal.db')
+    cursor = connection.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS alunos (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome TEXT,
+            turma INTEGER,
+            email TEXT,
+            senha INTEGER
+        )
+    ''')
+    connection.commit()
+    connection.close()
+
+@app.route('/alunos', methods=['GET', 'POST'])
+def professores():
+    connection = sqlite3.connect('Cantina_legal.db')
+    cursor = connection.cursor()
+
+    if request.method == 'POST':
+        nome = request.form['nome']
+        turma = request.form['']
